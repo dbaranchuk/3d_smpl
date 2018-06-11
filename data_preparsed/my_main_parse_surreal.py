45e1ab8b872a4446a56217fe959bb8e53cd85dca
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-12 02:04:40 +0300

    .

--- data_preparsed/my_main_parse_surreal.py
+++ data_preparsed/my_main_parse_surreal.py
@@ -13,7 +13,7 @@ dataset_name = 'surreal'
 # actions to parse
 # actors to parse for each action
 subjects = [10] #, 5, 6, 7, 8, 9, 11]
-subactions = ['04']
+subactions = ['04_f', '04_b']
 
 def check_dir(path):
   if not os.path.exists(path):
@@ -32,7 +32,6 @@ for subject in subjects:
             check_dir(output_folder)
 
             subfiles = [fname[:-4] for fname in os.listdir(data_folder) if fname.endswith('.mp4')]
-            print(os.listdir(data_folder))
             for sfile in subfiles:
                 #if "c0028" in sfile:
                 print sfile

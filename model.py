157851e017ac24760b3d8811d96141394bb2c63a
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-14 00:05:45 +0300

    .

--- model.py
+++ model.py
@@ -1134,9 +1134,10 @@ class _3DINN(object):
                                   self.resize_scale_sr_t, self.gender_sr_t, self.J_c_sr_t,
                                   self.idx_sr_t, self.pmesh_sr_t, self.v_gt_t])
 
+              v = None
+              J = None
               if self.is_unsup_train:
-                _, step, sup_loss, d3_loss, d2_loss, beta_, tf_vis \
-                   = self.sess.run([recon_optim, self.global_step, self.sup_loss, self.d3_loss, self.d2_loss, self.beta[0], self.tf_visibility],
+                  _, step, sup_loss, d3_loss, d2_loss, beta, v, J, tf_vis = self.sess.run([recon_optim, self.global_step, self.sup_loss, self.d3_loss, self.d2_loss, self.beta[0], self.v[0], self.J[0], self.tf_visibility],
                    feed_dict={self.beta_gt:batch_beta_t, self.pose_gt:batch_pose_t,
                          self.T_gt: batch_T_t, self.R_gt:batch_R_t,
                          self.gender_gt:batch_gender_t,

2ddd233b4b44978cf67a3b732102fb078554ed19
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-15 18:19:38 +0300

    .

--- data_preparsed/my_main_parse_surreal.py
+++ data_preparsed/my_main_parse_surreal.py
@@ -12,8 +12,8 @@ dataset_name = 'surreal'
 
 # actions to parse
 # actors to parse for each action
-subjects = [10] #, 5, 6, 7, 8, 9, 11]
-subactions = ['04_f', '04_b']
+subjects = ['02_01', '05_01', '06_01', 'ung_07_01', 'ung_07_03', 'ung_07_05', 'ung_07_08', 'ung_07_11', '08_01', '08_04', '08_05', '08_09', '08_11', '10_04', 'ung_12_01', 'ung_12_02', 'ung_12_03', '15_01', '26_01', '27_01', '37_01', '38_01', '38_02', '39_01', '39_02', '43_01', '45_01', 'ung_47_01', 'ung_49_01', '55_04', 'ung_74_01', 'ung_82_11', 'ung_82_12', 'ung_91_57', 'ung_104_02', 'ung_113_25', 'ung_132_18', 'ung_132_48', 'ung_120_20', 'ung_136_21', 'ung_139_28', '143_32']
+directions = ['f', 'b']
 
 def check_dir(path):
   if not os.path.exists(path):
@@ -23,8 +23,8 @@ full_data_path = os.path.join(data_root_dir, dataset_name)
 full_write_path = os.path.join(write_root_dir, dataset_name)
 
 for subject in subjects:
-    for subact in subactions:
-        filename = str(subject) + "_" + subact
+    for direction in directions:
+        filename = subject + "_" + direction
 
         if os.path.exists(os.path.join(full_data_path, filename)):
             data_folder = os.path.join(full_data_path, filename)
@@ -33,7 +33,6 @@ for subject in subjects:
 
             subfiles = [fname[:-4] for fname in os.listdir(data_folder) if fname.endswith('.mp4')]
             for sfile in subfiles:
-                #if "c0028" in sfile:
                 print sfile
                 parsed_data = get_training_params(sfile, data_dir = full_data_path)
                 write_syn_to_bin(parsed_data, os.path.join(output_folder, sfile) + ".bin")

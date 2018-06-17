8d06d8a9b37279f952c4ca59f10dd3ec9bd834f0
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-17 13:21:38 +0300

    .

--- pack_data/pack_gait_data_bin.py
+++ pack_data/pack_gait_data_bin.py
@@ -17,7 +17,7 @@ else:
     modality = 'train'
 
 data_path = os.path.join(data_path, modality)
-print data_pata
+print data_path
 
 filename = "/home/local/mocap/tf_code/gait/" + dataset + "/" + modality + ".tfrecords"
 convert_to_tfrecords_from_folder(data_path, filename, test=is_test, with_idx=with_idx, is_gait=True)

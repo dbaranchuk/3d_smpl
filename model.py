9208ee9458099b9faf2b5d2763a204f8b1e013e2
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-20 12:27:47 +0300

    .

--- model.py
+++ model.py
@@ -1081,8 +1081,8 @@ class _3DINN(object):
                             self.images:batch_image_t,
                             self.resize_scale_gt: batch_resize_scale_t})
 
-                            beta[idx_t[0]].append(batch_beta_t[0][frame_id])#_beta[0])
-                            pose[idx_t[0]].append(batch_pose_t[0][frame_id])#_pose[0])
+                            beta[idx_t[0]].append(_beta[0])# GT: batch_beta_t[0][frame_id])#_beta[0])
+                            pose[idx_t[0]].append(_pose[0])# GT: batch_pose_t[0][frame_id])
 
                 for i in beta.keys():
                     print(i)

5c30262562f86ba70d6f3e98c5b1962c91d42446
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-20 15:36:53 +0300

    .

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 
 #CUDA_VISIBLE_DEVICES=0 python main.py --name=model1 --model_dir=checkpoint/pretrain_b32_gf32_lr4_10k --is_train=True --learning_rate=0.000001  --batch_size=128 --gf_dim=32 --is_sup_train=True --max_iter=4000
 
-CUDA_VISIBLE_DEVICES=0 python main.py --name=model2 --model_dir=checkpoint/pretrain_b32_gf32_lr4_10k --is_train=True --learning_rate=0.00001  --batch_size=128 --gf_dim=32 --is_sup_train=True --max_iter=2000
+CUDA_VISIBLE_DEVICES=0 python main.py --name=model2 --model_dir=checkpoint/pretrain_b32_gf32_lr4_10k --is_train=True --learning_rate=0.000001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=5000
 
 #CUDA_VISIBLE_DEVICES=0 python main.py --name=overfit --model_dir=checkpoint/model0 --is_train=True --learning_rate=0.000001  --batch_size=32 --gf_dim=32 --is_sup_train=True --max_iter=2000
 

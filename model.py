da1930bfc4c6cb49fb703233e2319b06cc37513d
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-21 03:34:12 +0300

    .

--- model.py
+++ model.py
@@ -313,7 +313,7 @@ class _3DINN(object):
             self.centered_3d(*inputs_surreal(surreal_train_filenames, self.config.batch_size))
         #surreal_train_filenames = ["../tf_code/gait/surreal/train.tfrecords"]
         #self.pose_sr, self.beta_sr, self.T_sr, self.R_sr, self.J_sr, self.J_2d_sr, self.image_sr, self.seg_sr,\
-        self.chamfer_sr, self.c_sr, self.f_sr, self.resize_scale_sr, self.gender_sr, self.J_c_sr, self.idx_sr, self.pmesh_sr, self.v_gt_sr = self.centered_3d_with_idx(*inputs_surreal_with_idx(surreal_train_filenames, self.config.batch_size))
+        #self.chamfer_sr, self.c_sr, self.f_sr, self.resize_scale_sr, self.gender_sr, self.J_c_sr, self.idx_sr, self.pmesh_sr, self.v_gt_sr = self.centered_3d_with_idx(*inputs_surreal_with_idx(surreal_train_filenames, self.config.batch_size))
 
         # validation data from surreal
         surreal_test_filenames = ["../tf_code/tfrecords2/surreal2_100_test_quo1.tfrecords"]

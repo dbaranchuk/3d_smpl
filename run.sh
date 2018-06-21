551af9f99ab05ecf917cf9aa4af6eb70c0ccd42d
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-21 15:39:16 +0300

    .

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # pretrain
-CUDA_VISIBLE_DEVICES=0 nohup python main.py --name=pretrain_b64_gf16_lr4_10k --learning_rate=0.0001  --batch_size=64 --is_train=True --gf_dim=16 --is_sup_train=True --max_iter=10000
+CUDA_VISIBLE_DEVICES=0 python main.py --name=pretrain_b64_gf16_lr4_10k --learning_rate=0.0001  --batch_size=64 --is_train=True --gf_dim=16 --is_sup_train=True --max_iter=10000
 
 #CUDA_VISIBLE_DEVICES=0 python main.py --name=model0 --model_dir=checkpoint/pretrain_b32_gf32_lr4_10k --is_train=True --learning_rate=0.000001  --batch_size=128 --gf_dim=32 --is_sup_train=True --max_iter=2000
 

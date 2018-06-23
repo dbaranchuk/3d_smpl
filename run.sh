f2b2534c9b3f22fb317dddecaf58c79e7fbf5817
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-23 23:11:20 +0300

    .

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 # pretrain
 #CUDA_VISIBLE_DEVICES=0 python main.py --name=model0 --model_dir=checkpoint/pretrain_b32_gf32_lr4_10k --is_train=True --learning_rate=0.000001  --batch_size=128 --gf_dim=32 --is_sup_train=True --max_iter=2000
 
-CUDA_VISIBLE_DEVICES=0 python main.py --name=model5 --model_dir=checkpoint/pretrain_b32_gf32_lr4_10k --is_train=True --learning_rate=0.00001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
+#CUDA_VISIBLE_DEVICES=0 python main.py --name=model5 --model_dir=checkpoint/pretrain_b32_gf32_lr4_10k --is_train=True --learning_rate=0.00001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
 
-CUDA_VISIBLE_DEVICES=0 python main.py --name=model5 --model_dir=checkpoint/model5 --is_train=True --learning_rate=0.000001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
+#CUDA_VISIBLE_DEVICES=0 python main.py --name=model5 --model_dir=checkpoint/model5 --is_train=True --learning_rate=0.000001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
 
-CUDA_VISIBLE_DEVICES=0 python main.py --name=model5 --batch_size=1 --gf_dim=32 --is_sup_train=False --is_train=False
+#CUDA_VISIBLE_DEVICES=0 python main.py --name=model5 --model_dir=checkpoint/model5 --batch_size=1 --gf_dim=32 --is_sup_train=False --is_train=False
 
 #finetune
 CUDA_VISIBLE_DEVICES=0 python main.py --name=finetune --batch_size=1 --model_dir=checkpoint/model5 --gf_dim=32 --is_sup_train=False --is_train=True --key_loss=True --max_iter=242 #--silh_loss=True --max_iter=1000

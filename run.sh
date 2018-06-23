89763ee421f168959a0c63423b74d1cddb6b1d1d
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-24 02:20:44 +0300

    .

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@
 
 #CUDA_VISIBLE_DEVICES=0 python main.py --name=model6 --model_dir=checkpoint/model6 --is_train=True --learning_rate=0.000001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
 
-#CUDA_VISIBLE_DEVICES=0 python main.py --name=model6 --model_dir=checkpoint/model6 --batch_size=1 --gf_dim=32 --is_sup_train=False --is_train=False
+CUDA_VISIBLE_DEVICES=0 python main.py --name=model5 --model_dir=checkpoint/model5 --batch_size=1 --gf_dim=32 --is_sup_train=False --is_train=False
 
 #finetune
-CUDA_VISIBLE_DEVICES=0 python main.py --name=finetune --batch_size=1 --model_dir=checkpoint/model5 --gf_dim=32 --is_sup_train=False --is_train=True --key_loss=True --max_iter=1000 #--silh_loss=True --max_iter=1000
+#CUDA_VISIBLE_DEVICES=0 python main.py --name=finetune --batch_size=1 --model_dir=checkpoint/model5 --gf_dim=32 --is_sup_train=False --is_train=True --key_loss=True --max_iter=1000 #--silh_loss=True --max_iter=1000
 
 #CUDA_VISIBLE_DEVICES=0 python main.py --name=finetune --batch_size=1 --model_dir=checkpoint/finetune --gf_dim=32 --is_sup_train=False --is_train=False

52797901896fd599cb26732199ce15aa34695489
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-24 00:45:32 +0300

    .

--- pack_data/tfrecord_utils.py
+++ pack_data/tfrecord_utils.py
@@ -107,7 +107,7 @@ def loadBatchSurreal_fromString(file_string, image_size=128, num_frames=2, keypo
   # Cropping
   old_2d_center = np.array([(320 - 1)/2.0, (240-1)/2.0])
   # Use keypoint 0 in frame1 as center
-  J_2d = output[0]['J_2d']
+  J_2d = output[0]['J_2d'].astype('int32')
   import cv2
   img_path = '/home/local/tmp/'
   image = output[0]['image']

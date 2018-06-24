c2016e347fcc6eb4d23074642ac327e9076b3a1b
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-24 17:29:31 +0300

    .

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@
 #CUDA_VISIBLE_DEVICES=0 python main.py --name=model6 --model_dir=checkpoint/model6 --batch_size=1 --gf_dim=32 --is_sup_train=False --is_train=False
 
 #finetune
+rm -rf checkpoint/finetune/
 CUDA_VISIBLE_DEVICES=0 python main.py --name=finetune --batch_size=1 --model_dir=checkpoint/model6 --gf_dim=32 --is_sup_train=False --is_train=True --learning_rate=0.000001 --key_loss=True --max_iter=1000 #--silh_loss=True --max_iter=1000
 
 CUDA_VISIBLE_DEVICES=0 python main.py --name=finetune --batch_size=1 --model_dir=checkpoint/finetune --gf_dim=32 --is_sup_train=False --is_train=False

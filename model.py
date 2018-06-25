d1e749784b90809b0bc950a750ad52f02c40e2a1
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-25 20:09:29 +0300

    .

--- model.py
+++ model.py
@@ -1033,8 +1033,9 @@ class _3DINN(object):
                         self.images:batch_image_t,
                         self.resize_scale_gt: batch_resize_scale_t})
 
-                        #draw_2d_joints(batch_image_t[0][frame_id].copy()*255, batch_J_2d_t[0][frame_id], name='src_vis_'+str(frame_id)+'.jpg')
-                        #draw_2d_joints(batch_image_t[0][frame_id].copy()*255, _J_2d[0], name='mocap_vis'+str(frame_id)+'.jpg')
+                        draw_2d_joints(batch_image_t[0][frame_id].copy()*255, batch_J_2d_t[0][frame_id], name='src_vis_'+str(frame_id)+'.jpg')
+                        draw_2d_joints(batch_image_t[0][frame_id].copy()*255, _J_2d[0], name='mocap_vis'+str(frame_id)+'.jpg')
+                        draw_2d_joints(batch_seg_t[0][frame_id].copy()*255, _J_2d[0], name='mocap_seg'+str(frame_id)+'.jpg')
                         beta[idx_t[0]].append(_beta[0])
                         pose[idx_t[0]].append(_pose[0])
                         # GT

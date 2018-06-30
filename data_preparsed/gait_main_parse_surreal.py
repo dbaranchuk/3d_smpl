4108f6fa68ba7c4ffcfaf32201955ba31bc526f4
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-30 14:32:41 +0300

    .

--- data_preparsed/gait_main_parse_surreal.py
+++ data_preparsed/gait_main_parse_surreal.py
@@ -8,7 +8,7 @@ from write_utils import write_syn_to_bin, read_syn_to_bin
 
 data_root_dir = '/home/local/data/cmc/synthetic/gait_dataset_2' #run0'
 write_root_dir = '/home/local/data/cmc/synthetic/bin'
-is_test = True
+is_test = False
 
 if is_test:
     subjects = ['10_04']

9f8d0011f07b3ef85fb1b6be28800b71b9c26b9c
Dmitry <dmitrybaranchuk@gmail.com>
2018-06-30 23:11:28 +0300

    .

--- pack_data/pack_gait_data_bin.py
+++ pack_data/pack_gait_data_bin.py
@@ -9,7 +9,7 @@ from surreal_tfrecord_utils import convert_to_tfrecords_from_folder
 with_idx=True
 dataset = "surreal"
 data_path = "/home/local/data/cmc/synthetic/bin"
-is_test = False
+is_test = True
 modality = 'test' if is_test else 'train'
 
 data_path = os.path.join(data_path, modality)

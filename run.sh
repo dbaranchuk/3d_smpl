5e8bb2dcf4c40a44cab4c351b70df8381937e311
Dmitry <dmitrybaranchuk@gmail.com>
2018-07-01 22:38:30 +0300

    .

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 # pretrain
-#CUDA_VISIBLE_DEVICES=0 python main.py --name=model0 --model_dir=checkpoint/pretrain_b32_gf32_lr4_10k --is_train=True --learning_rate=0.000001  --batch_size=128 --gf_dim=32 --is_sup_train=True --max_iter=2000
+CUDA_VISIBLE_DEVICES=0 python main.py --name=model_from_sсratch --is_train=True --learning_rate=0.0001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=10000
 
-#CUDA_VISIBLE_DEVICES=0 python main.py --name=model9 --model_dir=checkpoint/pretrain_b32_gf32_lr4_10k --is_train=True --learning_rate=0.00001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
+#CUDA_VISIBLE_DEVICES=0 python main.py --name=model0 --model_dir=checkpoint/pretrain_b32_gf32_lr4_10k --is_train=True --learning_rate=0.00001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
 
-CUDA_VISIBLE_DEVICES=0 python main.py --name=model9 --model_dir=checkpoint/model9 --is_train=True --learning_rate=0.000001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
+CUDA_VISIBLE_DEVICES=0 python main.py --name=model_from_scratch --is_train=True --learning_rate=0.000001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
 
-CUDA_VISIBLE_DEVICES=0 python main.py --name=model9 --model_dir=checkpoint/model9 --batch_size=1 --gf_dim=32 --is_sup_train=False --is_train=False
+CUDA_VISIBLE_DEVICES=0 python main.py --name=model_from_scratch --batch_size=1 --gf_dim=32 --is_sup_train=False --is_train=False
 
 #finetune
 #rm -rf checkpoint/finetune1/

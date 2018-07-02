9a315db066ce340ea056dc3c5b8a3241149457c2
Dmitry <dmitrybaranchuk@gmail.com>
2018-07-03 01:49:07 +0300

    .

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 # pretrain
-CUDA_VISIBLE_DEVICES=0 python main.py --name=model_from_scratch --is_train=True --learning_rate=0.0001  --batch_size=32 --gf_dim=32 --is_sup_train=True --max_iter=1000
-
-CUDA_VISIBLE_DEVICES=0 python main.py --name=model_from_scratch --is_train=True --learning_rate=0.00001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=1000
-
-CUDA_VISIBLE_DEVICES=0 python main.py --name=model_from_scratch --is_train=True --learning_rate=0.000001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=1000
-
-CUDA_VISIBLE_DEVICES=0 python main.py --name=model_from_scratch --batch_size=1 --gf_dim=32 --is_sup_train=False --is_train=False
-
-
-#CUDA_VISIBLE_DEVICES=0 python main.py --name=op --model_dir=checkpoint/pretrain_b32_gf32_lr4_10k --is_train=True --learning_rate=0.00001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
+#CUDA_VISIBLE_DEVICES=0 python main.py --name=model_from_scratch --is_train=True --learning_rate=0.0001  --batch_size=32 --gf_dim=32 --is_sup_train=True --max_iter=1000
 #
-#CUDA_VISIBLE_DEVICES=0 python main.py --name=op --is_train=True --learning_rate=0.000001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
+#CUDA_VISIBLE_DEVICES=0 python main.py --name=model_from_scratch --is_train=True --learning_rate=0.00001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=1000
 #
-#CUDA_VISIBLE_DEVICES=0 python main.py --name=op --batch_size=1 --gf_dim=32 --is_sup_train=False --is_train=False
+#CUDA_VISIBLE_DEVICES=0 python main.py --name=model_from_scratch --is_train=True --learning_rate=0.000001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=1000
+#
+#CUDA_VISIBLE_DEVICES=0 python main.py --name=model_from_scratch --batch_size=1 --gf_dim=32 --is_sup_train=False --is_train=False
+
+
+CUDA_VISIBLE_DEVICES=0 python main.py --name=model0 --model_dir=checkpoint/pretrain_b32_gf32_lr4_10k --is_train=True --learning_rate=0.00001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
+
+CUDA_VISIBLE_DEVICES=0 python main.py --name=model0 --is_train=True --learning_rate=0.000001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
+
+CUDA_VISIBLE_DEVICES=0 python main.py --name=model0 --batch_size=1 --gf_dim=32 --is_sup_train=False --is_train=False
 
 #finetune
 #rm -rf checkpoint/finetune1/

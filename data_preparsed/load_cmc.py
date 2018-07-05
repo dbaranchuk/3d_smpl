d9ffefcf5a3aee0bb067bcaa3e32c0ad72b6c1f3
Dmitry <dmitrybaranchuk@gmail.com>
2018-07-05 04:09:09 +0300

    .

--- data_preparsed/load_cmc.py
+++ data_preparsed/load_cmc.py
@@ -50,7 +50,7 @@ def get_training_params(filename, data_dir, direction=None):
 
   for frame_id in range(num_frames):
     img = cap.get_data(frame_id)
-    #draw_2d_joints(np.array(img), d2.T, name='/home/local/tmp/dir/vis'+str(frame_id)+'.jpg')
+    draw_2d_joints(np.array(img), d2.T, name='/home/local/tmp/real/vis'+str(frame_id)+'.jpg')
     all_image[frame_id, :, :, :] = np.fliplr(img)
 
   output = dict()

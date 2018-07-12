9ebe7fc4da2c88f726d56fba33dedde350285c3f
Dmitry <dmitrybaranchuk@gmail.com>
2018-07-12 03:28:56 +0300

    .

--- model.py
+++ model.py
@@ -317,10 +317,10 @@ class _3DINN(object):
         self.pose_sr_v, self.beta_sr_v, self.T_sr_v, self.R_sr_v, self.J_sr_v, self.J_2d_sr_v, self.image_sr_v, self.seg_sr_v, self.chamfer_sr_v, self.c_sr_v, self.f_sr_v, self.resize_scale_sr_v, self.gender_sr_v, self.J_c_sr_v, self.idx_sr_v, self.pmesh_sr_v, self.v_gt_v = self.centered_3d_with_idx(*inputs_surreal_with_idx(surreal_valid_filenames, self.config.batch_size, shuffle=False, openpose=False))
 
         # test data to predict from surreal
-#        surreal_test_filenames = ["../tf_code/gait/surreal/test.tfrecords"]
-#        self.pose_sr_t, self.beta_sr_t, self.T_sr_t, self.R_sr_t, self.J_sr_t, self.J_2d_sr_t, self.image_sr_t, self.seg_sr_t, self.chamfer_sr_t, self.c_sr_t, self.f_sr_t, self.resize_scale_sr_t, self.gender_sr_t, self.J_c_sr_t, self.idx_sr_t, self.pmesh_sr_t, self.v_gt_t = self.centered_3d_with_idx(*inputs_surreal_with_idx(surreal_test_filenames, self.config.batch_size, shuffle=False, openpose=True))
-        real_test_filenames = ["../tf_code/gait/real/test.tfrecords"]
-        self.pose_sr_t, self.beta_sr_t, self.T_sr_t, self.R_sr_t, self.J_sr_t, self.J_2d_sr_t, self.image_sr_t, self.seg_sr_t, self.chamfer_sr_t, self.c_sr_t, self.f_sr_t, self.resize_scale_sr_t, self.gender_sr_t, self.J_c_sr_t, self.idx_sr_t, self.pmesh_sr_t, self.v_gt_t = self.centered_3d_with_idx(*inputs_cmc_with_idx(real_test_filenames, self.config.batch_size, shuffle=False))
+        surreal_test_filenames = ["../tf_code/gait/surreal/valid.tfrecords"]
+        self.pose_sr_t, self.beta_sr_t, self.T_sr_t, self.R_sr_t, self.J_sr_t, self.J_2d_sr_t, self.image_sr_t, self.seg_sr_t, self.chamfer_sr_t, self.c_sr_t, self.f_sr_t, self.resize_scale_sr_t, self.gender_sr_t, self.J_c_sr_t, self.idx_sr_t, self.pmesh_sr_t, self.v_gt_t = self.centered_3d_with_idx(*inputs_surreal_with_idx(surreal_test_filenames, self.config.batch_size, shuffle=False, openpose=False))
+#        real_test_filenames = ["../tf_code/gait/real/test.tfrecords"]
+#        self.pose_sr_t, self.beta_sr_t, self.T_sr_t, self.R_sr_t, self.J_sr_t, self.J_2d_sr_t, self.image_sr_t, self.seg_sr_t, self.chamfer_sr_t, self.c_sr_t, self.f_sr_t, self.resize_scale_sr_t, self.gender_sr_t, self.J_c_sr_t, self.idx_sr_t, self.pmesh_sr_t, self.v_gt_t = self.centered_3d_with_idx(*inputs_cmc_with_idx(real_test_filenames, self.config.batch_size, shuffle=False))
 
 
     def centered_3d(self, pose, beta, T, R, J, J_2d, image, seg, chamfer, c, f, resize_scale, gender):
@@ -1016,7 +1016,7 @@ class _3DINN(object):
         try:
             while not coord.should_stop():
                 beta, pose = {},{}
-                for i in range(60):#122):
+                for i in range(122): #60):
                     # load testing data
                     batch_pose_t, batch_beta_t, batch_T_t, batch_R_t, batch_J_t, batch_J_2d_t, batch_image_t, batch_seg_t, batch_chamfer_t, batch_c_t, batch_f_t, batch_resize_scale_t, batch_gender_t, batch_J_c_t, idx_t, batch_pmesh_t, batch_v_gt_t = self.sess.run([self.pose_sr_t, self.beta_sr_t, self.T_sr_t, self.R_sr_t, self.J_sr_t, self.J_2d_sr_t, self.image_sr_t, self.seg_sr_t, self.chamfer_sr_t, self.c_sr_t, self.f_sr_t, self.resize_scale_sr_t, self.gender_sr_t, self.J_c_sr_t, self.idx_sr_t, self.pmesh_sr_t, self.v_gt_t])
 

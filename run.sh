06ef7a9da3622017675cc2df41521b7fc5b521b3
Dmitry <dmitrybaranchuk@gmail.com>
2018-07-13 16:27:43 +0300

    .

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 # pretrain
-#CUDA_VISIBLE_DEVICES=0 python main.py --name=model_reconstruct_2d --model_dir=checkpoint/pretrain_b32_gf32_lr4_10k --is_train=True --learning_rate=0.00001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
+#CUDA_VISIBLE_DEVICES=0 python main.py --name=model_gt_2d --model_dir=checkpoint/pretrain_b32_gf32_lr4_10k --is_train=True --learning_rate=0.00001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
 
-#CUDA_VISIBLE_DEVICES=0 python main.py --name=model_reconstruct_2d --is_train=True --learning_rate=0.000001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
+#CUDA_VISIBLE_DEVICES=0 python main.py --name=model_gt_2d --is_train=True --learning_rate=0.000001  --batch_size=64 --gf_dim=32 --is_sup_train=True --max_iter=2000
 
-CUDA_VISIBLE_DEVICES=0 python main.py --name=model_reconstruct_2d --batch_size=1 --gf_dim=32 --is_sup_train=False --is_train=False
+CUDA_VISIBLE_DEVICES=0 python main.py --name=model_gt_2d --batch_size=1 --gf_dim=32 --is_sup_train=False --is_train=False
 
 #finetune
 #rm -rf checkpoint/finetune_real/
